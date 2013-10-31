57976c070b9b55132a501514df4b6c9d813c971e
Tomasz Socha <soczia@me.com>
2013-10-31 23:30:42 +0100

    rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,41 @@
 class Room < ActiveRecord::Base
     belongs_to :hotel, inverse_of: :rooms
     has_many :bookings
+    
   attr_accessible :beds, :hotel_id, :roomNumber, :roomtype
   
   def number_type
       "#{roomNumber} - #{roomtype}"
   end
+  
+  def booked_days
+        days = Array.new
+        bookings.where('room_id').each do| booking |
+            (booking.dateFrom..booking.dateTo).each do| day | 
+                days.push(day)
+            end
+        end
+        days
+  end
+  
+  def bookings_link
+      
+      links = Hash.new
+      bookings.where('room_id').each do| booking |
+          (booking.dateFrom..booking.dateTo).each do| day |
+              links[day]= booking
+          end
+      end
+      links
+  end
+  
+  def booked_days_test
+      
+      bookings.where('room_id')
+  end
+
+  def event
+      Event.first
+  end
+
 end
